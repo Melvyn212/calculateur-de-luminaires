b6349863cb97d356504ee48eed871f989ccac885
Melvyn212 <dehundeabdala@gmail.com>
2023-06-29 23:43:22 +0200

    ADD of scheme drawing

--- Version avec IHM/lineapp.py
+++ Version avec IHM/lineapp.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from line_window import LineWindow
 from tkinter import ttk
 from ttkthemes import ThemedTk
+import matplotlib.pyplot as plt
 
 
 class LineApp:
@@ -18,6 +19,8 @@ class LineApp:
 
         tk.Button(self.root, text="Ajouter une ligne", command=self.add_line).grid(row=0, column=0)
         tk.Button(self.root, text="Total", command=self.calculate).grid(row=0, column=1)
+        tk.Button(self.root, text="Afficher le schéma", command=self.draw_diagram).grid(row=1, column=1)
+
         self.label_cable = tk.Label(self.root, text="", font=("Helvetica", 12, "bold"), fg="red")
         self.label_cable.grid(row=2, column=0, columnspan=2)
         self.label_luminaire = tk.Label(self.root, text="", font=("Helvetica", 12, "bold"), fg="red")
@@ -38,6 +41,23 @@ class LineApp:
         total_luminaires = sum(line[3][1] for line in self.lines)
         self.label_cable["text"] = "Longueur totale de câble pour les luminaires : " + str(total_cable_length) + " m"
         self.label_luminaire["text"] = "Nombre total de luminaires : " + str(total_luminaires)
+
+    def draw_diagram(self):
+        plt.figure(figsize=(10,8))
+        for i, line in enumerate(self.lines, start=1):
+            # Représenter les lignes avec plt.plot()
+            plt.plot([0, line[0]], [i, i], color='blue')
+            
+            # Représenter les luminaires avec plt.scatter()
+            x = [line[2] + line[1] * n for n in range(line[3][1])]
+            y = [i] * line[3][1]  # Cette ligne place chaque ligne de luminaires à une hauteur différente sur le diagramme.
+            plt.scatter(x, y, color='red')
+        plt.title("Diagramme des lignes et des luminaires")
+        plt.xlabel("Distance (m)")
+        plt.ylabel("Numéro de ligne")
+        plt.show()
+
+
 
     @staticmethod
     def n_light(lenght, widht, w):
